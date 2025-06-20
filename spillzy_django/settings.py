229301fe911844260794d248dee29213d0901172
ikbalmed <mohamedikbal32@gmail.com>
2025-06-20 02:49:45 +0100

    Simple Changes

--- spillzy_django/settings.py
+++ spillzy_django/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'a-fallback-secret-key-for-development-only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'your-server-ip-address']
 # put your server IP address here
